54f266ea8924ce66d027637e8ddda15fcb4e9ac7
leandrobko <leandrobko@gmail.com>
2021-03-30 12:36:31 -0300

    Create PostCard test

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -30,8 +30,15 @@ describe('<Button />', () => {
   });
 
   it('should be enable when disabled is false', () => {
-    render(<Button text="Load more" disabled={false}/>);
+    const fn = jest.fn()
+    render(<Button text="Load more" disabled={false} onClick={fn}/>);
     const button = screen.getByRole('button', { name: /load more/i});
     expect(button).toBeEnabled()
+  });
+
+  it('should match snapshot', () => {
+    const fn = jest.fn()
+    const {container} = render(<Button text="Load more" disabled={false} onClick={fn}/>);
+    expect(container.firstChild).toMatchSnapshot()
   });
 })
---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { PostCard } from "."
+import { postCardPropsMock } from "./mock"
+
+const props = postCardPropsMock;
+
+describe('<PostCard />', () => {
+  it('should render PostCard correctly', () => {
+    render(<PostCard {...props}/>);
+    expect(screen.getByRole('img', { name: /title 1/i })).toHaveAttribute('src', 'img/img.png');
+    expect(screen.getByRole('heading', { name: /title 1/i })).toBeInTheDocument();
+    expect(screen.getByText('body 1')).toBeInTheDocument();
+  })
+
+  it('should match snapshot', () => { 
+    const {container} = render(<PostCard {...props}/>);
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
