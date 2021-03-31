effecd151e79314bde78436fa33d95a5ad2d13ab
leandrobko <leandrobko@gmail.com>
2021-03-31 12:45:39 -0300

    fix prettier

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -5,7 +5,7 @@ import './styles.css';
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
 import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput/indes';
+import { TextInput } from '../../components/TextInput';
 
 export const Home = () => {
   const [posts, setPosts] = useState([]);
